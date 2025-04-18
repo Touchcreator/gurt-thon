3bf6461c6197baeda263b114a71ade75c273fc76
TouchIsNotAGoodCoder <64277067+Touchcreator@users.noreply.github.com>
2025-04-18 18:11:12 -0400

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-true=True;false=False;yo=True;gurt=False;yap = print
-answer = input("should i remove true and false and just keep yo and gurt?")
-
-if answer.lower()=="y":
-    yap("feedback taken") # it isnt
-else:
-    yap("feedback taken, whyd you say otherwise tho") 
